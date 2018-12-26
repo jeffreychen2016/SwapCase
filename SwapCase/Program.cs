96364911c57e4677db7f4e31203ea259c816d642
Jeffrey Chen <jeffreychen2016@gmail.com>
2018-12-26 09:33:48 -0600

    finished

--- SwapCase/Program.cs
+++ SwapCase/Program.cs
@@ -6,7 +6,23 @@ namespace SwapCase
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var input = "HackerRank.com presents \"Pythonist 2\".";
+            var output = "";
+            foreach (char letter in input)
+            {
+                if (char.IsLower(letter))
+                {
+                    output += char.ToUpper(letter);
+                }
+                else
+                {
+                    output +=  char.ToLower(letter);
+                }
+
+
+            }
+
+            Console.WriteLine(output);
         }
     }
 }
